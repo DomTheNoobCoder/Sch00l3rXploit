3b82f766e5ef8fee10c1251c87595d18df23036f
DomTheTechMaster <101476659+DomTheNoobCoder@users.noreply.github.com>
2023-06-16 22:58:46 -0400

    deez

---
+++ index.js
@@ -0,0 +1,12 @@
+let isChromeDev = false;
+var adminManagmentApi = "";
+let isPatched = false;
+
+
+function exploit() 
+{
+    isChromeDev = true;
+    if (isPatched = true)  {
+     isChromeDev = false;
+    }
+}
